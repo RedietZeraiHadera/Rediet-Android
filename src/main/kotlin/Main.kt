ed93bfdf0e873b36cf25c6414c674c7cdaba7492
RedietZeraiHadera <redietzeraihadderra@gmail.com>
2023-02-03 07:27:35 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    var studentName = "Rediet Hadera"
+    println(studentName)
+
+var studentAge = 22
+    println(studentAge)
+
+    var studentPhone: Int = 251444862
+    println(studentPhone)
+
+    var studentWeight: Byte = 45
+    println(studentWeight)
+
+    var citizenKenya:Boolean = true
+    println(citizenKenya)
+
+
+}
